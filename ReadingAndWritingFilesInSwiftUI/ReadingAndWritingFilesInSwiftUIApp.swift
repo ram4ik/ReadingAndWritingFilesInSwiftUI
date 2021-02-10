f049570e3fda5bcf4bc945e73b0291bfc0f1b548
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-02-10 22:48:20 +0200

    Initial Commit

---
+++ ReadingAndWritingFilesInSwiftUI/ReadingAndWritingFilesInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadingAndWritingFilesInSwiftUIApp.swift
+//  ReadingAndWritingFilesInSwiftUI
+//
+//  Created by ramil on 10.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct ReadingAndWritingFilesInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
